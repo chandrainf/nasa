945a25bec2f8f809d556e8fdb0a118d267c6269c
Chandra Pratama <71168731+chandrainf@users.noreply.github.com>
2022-12-28 17:01:03 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=LehdOINU1tlJrQpdIZ7qXrjmlE5jtMIAZzjNz6MD;
         
         $("#loadImage").click(function(){
             $.ajax({
